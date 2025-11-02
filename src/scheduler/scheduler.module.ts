1244913e362a7b8fa8499c977e424cec2df90de4
ityn <ityn8161@gmail.com>
2025-11-02 21:26:25 +0700

    Fix scheduler not starting: add TelegrafModule import to SchedulerModule

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
 import { WorkShiftsModule } from '../database/models/work-shifts/work-shifts.module';
 import { ChatsModule } from '../database/models/chats/chats.module';
 import { PollsModule } from '../polls/polls.module';
 import { UsersModule } from '../database/models/users/users.module';
 
 @Module({
-  imports: [ConfigModule, WorkShiftsModule, ChatsModule, PollsModule, UsersModule],
+  imports: [
+    ConfigModule,
+    TelegrafModule,
+    WorkShiftsModule,
+    ChatsModule,
+    PollsModule,
+    UsersModule
+  ],
   providers: [SchedulerService],
   exports: [SchedulerService],
 })
